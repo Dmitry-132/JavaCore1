b5473f95e7dc54779b96c85472d7b006ba1e1aa7
Dimon <Dimon.1322@yandex.ru>
2026-07-03 22:43:04 +0300

    Java Collections Framework: Set

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,6 +1,9 @@
 package org.skypro.skyshop.product;
 
+import org.skypro.skyshop.Article.Article;
 import org.skypro.skyshop.Article.Searchable;
+
+import java.util.Objects;
 
 public abstract class Product implements Searchable {
     protected final String productName;
@@ -28,5 +31,17 @@ public abstract class Product implements Searchable {
     @Override
     public String typeContent() {
         return "< PRODUCT >";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return Objects.equals(searchTerm(), ((Product) o).searchTerm());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(searchTerm());
     }
 }
